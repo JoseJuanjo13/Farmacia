33d71db1a472b0a8167f6d4910915459406e767d
JoseJuanjo13 <juanj.hincapieo@uqvirtual.edu.co>
2022-11-07 12:30:27 -0500

    pantallas

--- farmacia/src/persistencia/Persistencia.java
+++ farmacia/src/persistencia/Persistencia.java
@@ -1,5 +1,11 @@
 package persistencia;
 
+import java.sql.Connection;
+
 public class Persistencia {
+
+	static ConexionSQL conexion = new ConexionSQL();
+	static Connection con = conexion.conexion();
+
 
 }
